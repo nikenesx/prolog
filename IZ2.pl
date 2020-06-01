da5f13b96499ca12e28d2612369d9a78f74d4f43
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2020-06-01 20:09:03 +0300

    Попытка реализовать поиск делителей числа

--- IZ2.pl
+++ IZ2.pl
@@ -1,17 +1,13 @@
-pr:-read(N),read_list(A,N),del(A,B),mop(B,C), write(C).
+iz2:-read(N),delit(N,H), write(H).
 
 read_list(A,N):-read_list([],A,0,N).
 read_list(A,A,N,N):-!.
 read_list(List,A,I,N):-	I1 is I+1,read(X), append(List,[X],List1), read_list(List1,A,I1,N).
 
-delit(0,0,[]):-!.
-delit(H,K,[A|B]):-K1 is K-1,.
-
-mop([],[]).
-mop([H|T],[X|Y]):-X is H, mop(T,Y).
-
-min([],[]):-!.
-min([H|T],[X|Y]):-delit(H,H,X), min(T,Y).
+delit(N,H):-delit_1(N,H,1).
+delit_1(N,_,C):-N=C.
+delit_1(N,[A|B],C):-N mod C = 0, A is C, C1 is C + 1, delit_1(N,B,C1).
+delit_1(N,L,C):-C1 is C+1, delit_1(N,L,C1).
 
 del([],[]).
-del([H|T],[X|Y]):-X is H/2, del(T,Y).
+del([H|T],[X|Y]):-d(2,H,X), del(T,Y).
