75e4dd97475a5ff53a5438f8dd991f882c1d2089
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2020-06-02 14:36:19 +0300

    Add files via upload

--- IZ2.pl
+++ IZ2.pl
@@ -1,6 +1,4 @@
-iz2:-read(N),read_list(A,N),new_list(A,B),razb(B,C),write(C).
-prime:-read(N),isPrime(N).
-bel:-read(N), notBelong(N,[1,2,3,4]).
+iz2:-read(N),read_list(A,N),new_list(A,B,R,[]), write(R).
 
 read_list(A,N):-read_list([],A,0,N).
 read_list(A,A,N,N):-!.
@@ -10,7 +8,8 @@ read_list(List,A,I,N):-
     read_list(List1,A,I1,N).
 
 notBelong(_,[]).
-notBelong(M,[X|Y]):- M \= X ,belong(X,Y).
+notBelong(M,[X|Y]):-M \= X, notBelong(M,Y).
+
 
 isPrime(X):-isPrime_1(X,2).
 isPrime_1(X,X).
@@ -20,22 +19,22 @@ isPrime_1(X,P):-
     P1 is P+1,
     isPrime_1(X,P1).
 
-delit(N,H):-delit_1(N,H,2).
+delit(N,B):-delit_1(N,B,2).
 delit_1(N,[],N).
-delit_1(N,[A|B],C):-
-    Temp is N mod C,
+delit_1(N,[X|Y],I):-
+    Temp is N mod I,
     Temp = 0,
-    isPrime(C),
-    notBelong(C,[A|B]),
-    A is C,
-    C1 is C + 1,
-    delit_1(N,B,C1).
+    isPrime(I),
+    X is I,
+    I1 is I + 1,
+    delit_1(N,Y,I1).
 
-delit_1(N,L,C):-
-    C1 is C+1,
-    delit_1(N,L,C1).
+delit_1(N,B,I):-
+    I1 is I+1,
+    delit_1(N,B,I1).
 
-new_list([],[]).
-new_list([H|T],[X|Y]):-
+new_list([],[],R,RAcc) :- R = RAcc, !.
+new_list([H|T],[X|Y],R, RAcc):-
     delit(H,X),
-    new_list(T,Y).
+    append(RAcc, X, R1),
+    new_list(T,Y,R,R1).
