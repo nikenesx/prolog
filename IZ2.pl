77b28194b16eb21d18cccbf2295a5e3030279801
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2020-06-04 17:14:34 +0300

    ИЗ2

--- IZ2.pl
+++ IZ2.pl
@@ -1,4 +1,4 @@
-iz2:-read(N),read_list(A,N),new_list(A,B,R,[]), write(R).
+iz2:-read(N),read_list(A,N),new_list(A,[],Result),delR(Result,R), write(R).
 
 read_list(A,N):-read_list([],A,0,N).
 read_list(A,A,N,N):-!.
@@ -7,9 +7,16 @@ read_list(List,A,I,N):-
     append(List,[X],List1),
     read_list(List1,A,I1,N).
 
-notBelong(_,[]).
-notBelong(M,[X|Y]):-M \= X, notBelong(M,Y).
+delR([],[]).
+delR([H|T],[H|Y]):-
+    delE(H,T,R),!,
+    delR(R,Y).
 
+delE(_,[],[]).
+delE(H,[H|T],R):-!,
+    delE(H,T,R).
+delE(H,[X|T],[X|R]):-!,
+    delE(H,T,R).
 
 isPrime(X):-isPrime_1(X,2).
 isPrime_1(X,X).
@@ -19,22 +26,24 @@ isPrime_1(X,P):-
     P1 is P+1,
     isPrime_1(X,P1).
 
-delit(N,B):-delit_1(N,B,2).
-delit_1(N,[],N).
-delit_1(N,[X|Y],I):-
-    Temp is N mod I,
+delit(H,C):-delit_1(H,_,2,C).
+delit_1(H,B,H,C):-C=B.
+delit_1(H,B,I,C):-
+    Temp is H mod I,
     Temp = 0,
     isPrime(I),
-    X is I,
+    append(B,[I],B1),
     I1 is I + 1,
-    delit_1(N,Y,I1).
+    delit_1(H,B1,I1,C).
 
-delit_1(N,B,I):-
+delit_1(H,B,I,C):-
     I1 is I+1,
-    delit_1(N,B,I1).
+    delit_1(H,B,I1,C).
 
-new_list([],[],R,RAcc) :- R = RAcc, !.
-new_list([H|T],[X|Y],R, RAcc):-
-    delit(H,X),
-    append(RAcc, X, R1),
-    new_list(T,Y,R,R1).
+new_list([],Result,Result2):-Result2 = Result.
+new_list([H|T],Result,Result2):-
+    delit(H,Temp1),
+    append(Temp1,Result,ResultT),
+    new_list(T,ResultT,Result2).
+
+
