98e1595694e91aeea059a88a1ae99a69e88921a2
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2020-06-06 00:20:48 +0300

    Add files via upload

--- ИЗ1.pl
+++ ИЗ1.pl
@@ -1,6 +1,6 @@
 iz1:-start(Res),write(Res).
-d:-X is 1/3,name(X,X1),pr(X1,D),write(D).
-pr([V1,V2|T],D):-append(T,[],D).
+
+d:-search([1,2,3],X),write(X).
 
 ifMore(R1,Res,Res1):-
     R1 > Res,
@@ -8,12 +8,13 @@ ifMore(R1,Res,Res1):-
 ifMore(_,Res,Res1):-
     Res1 = Res.
 
-search([],_).
-search([H|T],R1):-H = 46, search_1(T,R1).
-search([_|T],R1):-search(T,R1).
+countElem([],_).
+countElem([_|T],X):-X1 is X+1,countElem(T,X1).
 
+search([_,_|T],R1):-search_1(T,R1,[],T).
+search_1([H|_],R1,Per,TS):-append(H,Per,Per1), isPeriod(Per1,TS),X is 0,countElem(Per1,X),R1 = X.
+search_1([_|T],R1,Per,TS):-search(T,R1,Per,TS).
 
-start(Res):-start_1(Res,1000,2).
 start_1(_,N,N).
 start_1(Res,N,I):-
     Fr is 1/N,
@@ -22,4 +23,4 @@ start_1(Res,N,I):-
     ifMore(R1,Res,Res1),
     I1 is I + 1,
     start_1(Res1,N,I1).
-
+start(Res):-start_1(Res,1000,2).
