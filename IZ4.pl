e1a4732cd3f40b2fb8e6421c141b51d88dda9bec
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2020-06-08 19:06:41 +0300

    Add files via upload

--- IZ4.pl
+++ IZ4.pl
@@ -6,10 +6,17 @@ student("Роман").
 student("Петр").
 student("Сергей").
 
+in_list([H|_],H).
+in_list([_|T],H):-in_list(T,H).
+
 launch:-student(X),
 	student(Y),
 	student(Z),
+
 	not(X = Y),
 	not(X = Z),
 	not(Y = Z),
+
+        Data = [[X, "Физический"], [Y, "Математический"], [Z, "Химический"]],
+
 
