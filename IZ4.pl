ba020365d0620c9f1959e6d454e260e7844dfc0d
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2020-06-09 00:34:35 +0300

    Add files via upload

--- IZ4.pl
+++ IZ4.pl
@@ -9,14 +9,30 @@ student("Сергей").
 in_list([H|_],H).
 in_list([_|T],H):-in_list(T,H).
 
-launch:-student(X),
-	student(Y),
-	student(Z),
+iz4:-
+    student(X),
+    student(Y),
+    student(Z),
 
-	not(X = Y),
-	not(X = Z),
-	not(Y = Z),
+    not(X = Y),
+    not(X = Z),
+    not(Y = Z),
 
-        Data = [[X, "Физический"], [Y, "Математический"], [Z, "Химический"]],
+    Data = [[X, "Физический"], [Y, "Математический"], [Z, "Химический"]],
+
+    (
+        ((in_list(Data, ["Петр", "Математический"]),
+        not(in_list(Data, ["Сергей", "Физический"])));
+        not(in_list(Data, ["Петр", "Математический"]))),
+
+        ((not(in_list(Data, ["Роман", "Физический"])),
+        in_list(Data, ["Петр", "Математический"]));
+        in_list(Data, ["Роман", "Физический"])),
+
+        ((not(in_list(Data, ["Сергей", "Математический"])),
+        in_list(Data, ["Роман", "Химический"]));
+        in_list(Data, ["Сергей", "Математический"]))
+    ),
+    write(Data), !.
 
 
