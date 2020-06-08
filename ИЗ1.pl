48a8400b058e21dee939bdc4edac192399ceae0a
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2020-06-08 20:18:29 +0300

    Add files via upload

--- ИЗ1.pl
+++ ИЗ1.pl
@@ -1,4 +1,12 @@
-iz1:-start(Res),write(Res).
+iz1:-start(Res),pp(Res,Per),write(Per).
+
+pp(Res,Per):-pp_1(Res,Per,0,_).
+pp_1([],Per,_,C):-Per = C.
+pp_1([[V1,V2]|T],Per,I,_):-
+    V2 > I,
+    pp_1(T,Per,V2,V1).
+pp_1([_|T],Per,I,C):- pp_1(T,Per,I,C).
+
 
 isPeriod(List,List2):-isPeriod_1(List,List2,List2,0).
 isPeriod_1(_,_,_,2).
@@ -13,31 +21,33 @@ isPeriod_1([H|T],[X|Y],Per1,I):-
     isPeriod_1(T,Y,Per1,I).
 
 searchPeriod([_,_|Tail],R1):-searchPeriod_1(Tail,[],R1,0,Tail).
-searchPeriod_1([],_,R1,I,_):- R1 = I.
-searchPeriod_1([H|_],Acc,R1,I,Tail):-
+searchPeriod_1(_,25,R1,I,_):-R1 = I.
+searchPeriod_1([],_,_,_,_):- fail.
+searchPeriod_1([H|T],Acc,R1,I,Tail):-
     I < 9,
     append(Acc,[H],Acc1),
     I1 is I+1,
+    T\=[],
     isPeriod(Tail,Acc1),
-    searchPeriod_1([],Acc,R1,I1,Tail).
+    searchPeriod_1([],25,R1,I1,Tail).
 searchPeriod_1([H|T],Acc,R1,I,Tail):-
-    I < 9,
+    I < 10,
     append(Acc,[H],Acc1),
     I1 is I + 1,
     searchPeriod_1(T,Acc1,R1,I1,Tail).
 
-start_1(_,N,N,Nn,T):-Nn = T.
-start_1(Res,N,I,Nn,_):-
+start_1(R,N,N,Nn,_):-Nn = R.
+start_1(R,N,I,Nn,_):-
     Fr is 1/I,
     name(Fr,FrList),
     searchPeriod(FrList,R1),
-    R1 > Res,
+    append(R,[[I,R1]],K),
     Tt = Fr,
     I1 is I + 1,
-    start_1(R1,N,I1,Nn,Tt).
+    start_1(K,N,I1,Nn,Tt).
 start_1(Res,N,I,Nn,T):-
     I1 is I + 1,
     start_1(Res,N,I1,Nn,T).
 
-start(Nn):-start_1(0,10,7,Nn,_).
+start(Nn):-start_1([],1000,2,Nn,_).
 
