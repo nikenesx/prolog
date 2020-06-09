63ee590a830bbc69e48500f070eff0a931f5aa58
Nikita Gritsanenko <58622128+nikenesx@users.noreply.github.com>
2020-06-09 22:00:01 +0300

    Add files via upload

--- IZ7.pl
+++ IZ7.pl
@@ -1,5 +1,23 @@
 :- dynamic car/7.
 
+car("Infinity QX60", 2, 1, 5, 1, 2, 1).
+car("Volkswagen Tiguan", 2, 1, 4, 2, 3, 1).
+car("Lada Granta", 2, 1, 1, 2, 1, 1).
+car("Lada Priora", 2, 2, 2, 2, 2, 1).
+car("Audi Q5", 2, 1, 4, 2, 3, 1).
+car("КамАЗ-65207", 3, 1, 5, 2, 2, 1).
+car("ТОНАР-7502", 3, 2, 5, 2, 3, 1).
+car("MAN TGS", 3, 2, 5, 2, 2, 1).
+car("Navistar 9000", 3, 2, 5, 3, 1, 1).
+car("ПАЗ-3205 (классический 80-х годов)", 4, 2, 5, 2, 1).
+car("КамАЗ-6282", 4, 2, 5, 2, 3, 2).
+car("Tesla Roadster", 2, 2, 5, 3, 2).
+car("Tesla Cybertruck", 2, 1, 4, 3, 2).
+car("Chevrolet Volt", 2, 1, 2, 3, 2, 3).
+car("Ford Fusion", 2, 1, 2, 3, 2, 3).
+car("Honda Insight", 1, 1, 2, 1, 2, 3).
+car("Lexus RX 450h", 2, 1, 4, 2, 3, 3).
+
 iz7:-
     write("Какая категория вашего автомобиля?"), nl,
     write("1. A"), nl,
@@ -55,3 +73,24 @@ iz7:-
 	write("Вашего автомобиля нет в базе данных. Введите название:"), read(NewCarName),
 	assert(car(NewCarName, Category, IsAliveNow, BodyType, RegionOfProduction, Composition, MotorType)),
 	write("Автомобиль добавлен в БД!"), save('c:/Users/who/Desktop/db.pl').
+
+save(Filename) :-
+    tell(Filename),
+    forall(
+                (car(Name, Category, IsAliveNow, BodyType, RegionOfProduction, Composition, MotorType)),
+		(write(":- car(\""),
+                 write(Name),
+                 write("\", "),
+                 write(Category),
+                 write(", "),
+                 write(IsAliveNow),
+                 write(", "),
+                 write(BodyType),
+                 write(", "),
+                 write(RegionOfProduction),
+                 write(", "),
+                 write(Composition),
+                 write(", "),
+                 write(MotorType),
+                 write(")."), nl)
+	), told.
